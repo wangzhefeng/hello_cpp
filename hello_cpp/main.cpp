2b45d659374dec7c1fbd823b3d87a86b1f1e5cb8
wangzhefeng <wangzhefengr@163.com>
2020-12-01 22:43:30 +0800

    Initial Commit

---
+++ hello_cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hello_cpp
+//
+//  Created by wangzhefeng on 2020/12/1.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
