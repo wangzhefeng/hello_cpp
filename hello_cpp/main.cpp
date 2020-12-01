31a6cc56b7c63f33a1819cf9d13dddee1eafebf4
wangzhefeng <wangzhefengr@163.com>
2020-12-01 23:18:37 +0800

    init project

--- hello_cpp/main.cpp
+++ hello_cpp/main.cpp
@@ -6,9 +6,23 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    // 在屏幕中输出 Hello World!
     std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!\n" << endl;
+    
+    // --------------------
+    // 1.注释
+    // --------------------
+    // (1)单行注释
+    
+    /*
+     (2)TODO
+     需要查询资料进行确认为什么不可以执行
+     */
+     // system("pause");
+    
     return 0;
 }
